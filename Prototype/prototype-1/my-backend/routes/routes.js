ccbe3fbca3f79fc70a160e2aa648a5d6618f5cae
Shahrexz <shahrez.faisal89@gmail.com>
2024-11-10 23:21:00 +0500

    prototype

---
+++ Prototype/prototype-1/my-backend/routes/routes.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const authController = require('../controllers/authController');
+const reviewController = require('../controllers/reviewController');
+
+const router = express.Router();
+router.post('/signup', authController.signup);
+router.post('/login', authController.login);
+router.get('/Reviews', reviewController.getReviews);
+router.post('/Reviews', reviewController.createReview);
+
+module.exports = router;
